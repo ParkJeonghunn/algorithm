4c7101a3a7906ec177e8bd81dea433d05404c7da
Parkjeonghunn <gkrwkd95@gmail.com>
2022-01-20 22:24:52 +0900

    22.01.20 algorithm

---
+++ 2884.py
@@ -0,0 +1,11 @@
+h,m = map(int, (input().split(" ")))
+0<=h<=23, 0<=m<=59
+
+if h >=1 and m>=45:
+    print(h, m-45)
+elif h >=1:
+    print(h-1, 60-(45-m))
+elif h ==0 and m>=45:
+    print(h, m-45)
+else:
+    print(23, 60-(45-m))
